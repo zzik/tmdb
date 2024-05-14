6e43b1acbd570f6b004304612eeb6d084f1a6bfe
zzik <edin.base@outlook.com>
2024-05-14 14:06:13 +0200

    implementing MUI component library

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,24 @@
 import { array, bool } from "prop-types";
 import MovieCard from "./MovieCard";
+import { ImageList } from "@mui/material";
 
 const MovieList = ({ loading, error, movies }) => {
   if (loading && !error) return <div>Loading...</div>;
   if (!loading && error) return <div>error occurred.unable to load movies</div>;
   if (!movies) return null;
   return (
-    <ul>
+    <ImageList cols={2}>
+
       {movies.slice(0, 10).map((movie) => (
-        <MovieCard key={movie.id} id={movie.id} title={movie.title} overview={movie.overview} />
+        <MovieCard
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          overview={movie.overview}
+          image_path={movie.backdrop_path}
+        />
       ))}
-    </ul>
+    </ImageList>
   );
 };
 MovieList.propTypes = {
