e8cb688d26656c1130cb3e6052aa9881f47004d3
zzik <edin.base@outlook.com>
2024-05-14 14:04:31 +0200

    implementing MUI and developing movie card logic

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,32 @@
+import { IconButton, ImageListItem, ImageListItemBar } from "@mui/material";
 import { number, string } from "prop-types";
 import { Link } from "react-router-dom";
 
-const MovieCard = ({ id, title, overview }) => {
+const MovieCard = ({ id, title, overview, image_path }) => {
   return (
     <Link to={`/movie/${id}`}>
-      <p>{title}</p>
-      <p>{overview}</p>
-      <hr/>
+      <ImageListItem >
+      <ImageListItemBar
+            title={title}
+            subtitle={overview}
+            actionIcon={
+              <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                aria-label={`info about ${title}`}
+              >
+              </IconButton>
+            }
+          />
+        <img loading="lazy" alt={`${overview}`} src={`https://image.tmdb.org/t/p/w780/${image_path}`} />
+
+      </ImageListItem>
     </Link>
   );
 };
 MovieCard.propTypes = {
   title: string,
   id: number,
-  overview: string
+  overview: string,
+  image_path: string,
 };
 export default MovieCard;
