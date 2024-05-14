77144ce85c216d33d60e32676b4358911eb89775
zzik <edin.base@outlook.com>
2024-05-14 14:05:53 +0200

    removing fetching logic

--- src/components/MoviesContainer.jsx
+++ src/components/MoviesContainer.jsx
@@ -1,40 +1,11 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import MovieList from "./MovieList";
+import {useFetchMovies} from '../hooks/useFetchMovies'
 
 const MoviesContainer = () => {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const getMovies = async () => {
-    setIsLoading(true);
-    try {
-      const data = await axios.get(
-        "https://api.themoviedb.org/3/movie/top_rated",
-        {
-          params: {
-            api_key: "516bd90eaa5d6fc72dd79e7ba1bfaa28",
-          },
-        }
-      );
-      // console.log(data.data.results)
-      setIsLoading(false);
-      if (!data) return;
-      setMovies(data.data.results);
-    } catch (err) {
-      setError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
+  const [movies, loading, error] = useFetchMovies()
 
   return (
-    <MovieList loading={isLoading} error={error} movies={movies} />
+    <MovieList loading={loading} error={error} movies={movies} />
   );
 };
 
