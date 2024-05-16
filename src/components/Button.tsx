578c063290754ddbae9c263b21f5fc6db6b34ac6
zzik <edin.base@outlook.com>
2024-05-16 08:41:33 +0200

    refactored static button component into dynamic react component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonT } from "../types"
+
+const Button = ({callback, disabled, text}:ButtonT) => {
+  return (
+    <button onClick={() => {callback()}} disabled={disabled}>
+        {text}
+    </button>
+  )
+}
+
+export default Button
