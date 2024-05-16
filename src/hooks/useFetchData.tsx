52278d8b8424043e2788bfbb35e3a59edc330de2
zzik <edin.base@outlook.com>
2024-05-16 08:37:25 +0200

    implemented environment variable and resolved ESLint warning

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,30 +1,32 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export const useFetchData = (endpoint:string) => {
+const useFetchData = (endpoint: string) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  async function fetchData() {
-    setLoading(true);
-    try {
-      const response = await axios.get(endpoint, {
-        params: {
-          api_key: "516bd90eaa5d6fc72dd79e7ba1bfaa28",
-        },
-      });
-      setLoading(false);
-      setData(response.data.results);
-    } catch (err) {
-      setError(true);
-      setLoading(false);
-    }
-  }
-
   useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(endpoint, {
+          params: {
+            api_key: import.meta.env.VITE_API_KEY,
+          },
+        });
+        setLoading(false);
+        setData(response.data.results);
+      } catch (err) {
+        setError(true);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
     fetchData();
-  }, []);
+  }, [endpoint]);
 
   return [data, loading, error];
 };
+export default useFetchData;
