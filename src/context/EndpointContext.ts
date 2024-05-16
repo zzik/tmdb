fc5dcf0ff5f7f6d7c34ea61eee00b66885eb637d
zzik <edin.base@outlook.com>
2024-05-16 08:52:14 +0200

    created context for axios endpoint handling

---
+++ src/context/EndpointContext.ts
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const EndpointContext = createContext<{
+  baseLink: string;
+  setBaseLink: (baseLink: string) => void
+}>({
+  baseLink: "https://api.themoviedb.org/3",
+  setBaseLink: () => {},
+});
+
