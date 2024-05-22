d703ad1657ccb38c63e738cbaab2d0b4ce8120e6
zzik <edin.base@outlook.com>
2024-05-22 07:03:12 +0200

    centralized exports for types folder

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+import { MediaState, MediaT, RouteState, ButtonT } from "./components";
+import { QueryHandlerT, EndpointGeneratorT } from "./utils";
+import { QueryContextT, TypeContextT, EndpointContextT } from "./context";
+
+export type {
+  MediaState,
+  MediaT,
+  RouteState,
+  QueryHandlerT,
+  EndpointGeneratorT,
+  ButtonT,
+  TypeContextT,
+  QueryContextT,
+  EndpointContextT,
+};
