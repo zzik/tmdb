755752adddb03a5d3c70ca37c00c9dc27468728f
zzik <edin.base@outlook.com>
2024-05-22 03:52:23 +0200

    imported hook for rendering essential app states

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Display from "./components/Display";
 import { Route, Routes } from "react-router-dom";
 import RouteContainer from "./components/Route/RouteContainer";
 import { TypeContext, EndpointContext, QueryContext } from "./context";
+import { useDisplayState } from "./hooks";
 
 function App() {
 
