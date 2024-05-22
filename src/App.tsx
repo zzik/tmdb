bf1a90fb400fb162bb64980e4bb28b4595874eeb
zzik <edin.base@outlook.com>
2024-05-22 03:50:53 +0200

    imported context reference components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Display from "./components/Display";
 import { Route, Routes } from "react-router-dom";
 import RouteContainer from "./components/Route/RouteContainer";
+import { TypeContext, EndpointContext, QueryContext } from "./context";
 
 function App() {
 
