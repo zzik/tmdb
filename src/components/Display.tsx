3858e03533819ee8fdfa10b8fc590167f5d8b3fd
zzik <edin.base@outlook.com>
2024-05-22 03:39:46 +0200

    moved context provider wrappers out of the main display component

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,22 +1,14 @@
-import { TypeContext, EndpointContext, QueryContext } from "../context";
 import SearchBar from "./SearchBar";
 import ContentTypeSwitch from "./ContentTypeSwitch";
-import MediaContainer from "../MediaContainer";
-import { useDisplayState } from "../hooks";
+import MediaContainer from "./Media/MediaContainer";
 
 const Display = () => {
-  const { type, setType, query, setQuery, queryMode, setQueryMode, baseLink, setBaseLink } = useDisplayState();
-  
   return (
-    <QueryContext.Provider value={{ query, setQuery, queryMode, setQueryMode }}>
-      <TypeContext.Provider value={{ type, setType }}>
-        <EndpointContext.Provider value={{ baseLink, setBaseLink }}>
-          <SearchBar />
-          <ContentTypeSwitch />
-          <MediaContainer />
-        </EndpointContext.Provider>
-      </TypeContext.Provider>
-    </QueryContext.Provider>
+    <>
+      <SearchBar />
+      <ContentTypeSwitch />
+      <MediaContainer />
+    </>
   );
 };
 
