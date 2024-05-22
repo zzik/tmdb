fffd01ee6d4943be99c235bb0fb8bf8d6b8621f5
zzik <edin.base@outlook.com>
2024-05-22 03:44:39 +0200

    removed font and browser router imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { BrowserRouter } from "react-router-dom";
-import Display from './components/Display';
+import Display from "./components/Display";
 
 function App() {
 
