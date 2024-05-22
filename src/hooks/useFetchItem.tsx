ba69595184d5c73bbce646a081a3e7cf1d7be6d2
zzik <edin.base@outlook.com>
2024-05-22 07:04:59 +0200

    renaming hooks and hook files

--- src/hooks/useFetchItem.tsx
+++ src/hooks/useFetchItem.tsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const useFetchRoute = (route: string) => {
+// const YOUR_API_KEY = import.meta.env.VITE_API_KEY
+const YOUR_API_KEY = "516bd90eaa5d6fc72dd79e7ba1bfaa28"
+
+const useFetchItem = (route: string) => {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    const fetchRoute = async () => {
+    const fetchItem = async () => {
       setLoading(true);
       try {
         const response = await axios.get(route, {
           params: {
-            api_key: "516bd90eaa5d6fc72dd79e7ba1bfaa28",
+            api_key: YOUR_API_KEY,
           },
         });
         setLoading(false);
@@ -24,9 +27,9 @@ const useFetchRoute = (route: string) => {
         setLoading(false);
       }
     };
-    fetchRoute();
+    fetchItem();
   }, [route]);
 
   return [data, loading, error];
 };
-export default useFetchRoute;
+export default useFetchItem;
