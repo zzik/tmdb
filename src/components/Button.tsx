0c927729c912d07cd419810415da313062cca17f
zzik <edin.base@outlook.com>
2024-05-22 03:35:46 +0200

    applying code formatter, minor aesthetic changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,16 @@
-import { ButtonT } from "../types"
+import { ButtonT } from "../types";
 
-const Button = ({callback, disabled, text}:ButtonT) => {
+const Button = ({ callback, disabled, text }: ButtonT) => {
   return (
-    <button onClick={() => {callback()}} disabled={disabled}>
-        {text}
+    <button
+      onClick={() => {
+        callback();
+      }}
+      disabled={disabled}
+    >
+      {text}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
