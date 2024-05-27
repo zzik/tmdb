b80c53feb22b04a99d69721ea1ee45882f7a2300
zzik <edin.base@outlook.com>
2024-05-27 11:28:04 +0200

    implementing useImage hook

--- src/components/Media/MediaCard.tsx
+++ src/components/Media/MediaCard.tsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
-import { MediaData } from "../../types";
+import { MediaT } from "../../types";
 import { useContext } from "react";
 import { TypeContext } from "../../context";
+import useImagePath from "../../hooks/useImagePath";
 
-const MediaCard = ({ id, title, overview, image_path }: MediaData) => {
-  const imagePath = image_path
-    ? `https://image.tmdb.org/t/p/w780/${image_path}`
-    : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7nlpbOs-w7q6dGxP5zYBgSeiX7pUAh-0QdDWS-KW1k_wxnv9Obw5B7NaEMwCOwfTwDWs&usqp=CAU";
-  const typeContext = useContext(TypeContext);
+const MediaCard = ({
+  id,
+  name,
+  title,
+  overview,
+  backdrop_path,
+  poster_path,
+}: MediaT) => {
+  const { type } = useContext(TypeContext);
+  const imagePath = useImagePath({ backdrop_path, poster_path });
+  const media_title = title === undefined ? name : title;
   return (
-    <Link to={`/${typeContext.type}/${id}`}>
-      <img loading="lazy" alt={`${overview}`} src={imagePath} />
-      <p>{title}</p>
+    <Link to={`/${type}/${id}`}>
+      <img loading="lazy" alt={`${overview}`} src={`${imagePath}`} />
+      <p>{media_title}</p>
     </Link>
   );
 };
