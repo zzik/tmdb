19bdcf0772bb828ce711d1b57d4ac68c0f88756b
zzik <edin.base@outlook.com>
2024-05-28 01:01:08 +0200

    added app title component

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,10 +1,12 @@
 import SearchBar from "./SearchBar";
 import ContentTypeSwitch from "./ContentTypeSwitch";
 import { MediaContainer } from "./Media";
+import Title from "./Title";
 
 const Display = () => {
   return (
     <main className="display">
+      <Title />
       <SearchBar />
       <ContentTypeSwitch />
       <MediaContainer />
---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+const Title = () => {
+  return (
+    <div className='app-title'>
+        <h1>Movie Lookup</h1>
+    </div>
+  )
+}
+
+export default Title
