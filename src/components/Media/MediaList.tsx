f3fa30b5593bce33e6ca23b28ed2fd358a608c5d
zzik <edin.base@outlook.com>
2024-05-27 11:27:23 +0200

    increasing amount of results after search

--- src/components/Media/MediaList.tsx
+++ src/components/Media/MediaList.tsx
@@ -1,19 +1,27 @@
 import MediaCard from "./MediaCard";
 import { MediaState } from "../../types";
+import { useContext } from "react";
+import { QueryContext } from "../../context";
 
-const MediaList = ({ loading, error, media }: MediaState) => {
+const MediaList = ({ loading, error, data }: MediaState) => {
+  const queryContext = useContext(QueryContext)
+  
   if (loading && !error) return <div>Loading...</div>;
   if (!loading && error) return <div>Error occurred. Unable to load media</div>;
-  if (!media) return null;
+  if (!data) return null;
+  
+  const activeSearch = queryContext.queryMode ? data.length : 10
+
   return (
     <div>
-      {media.slice(0, 10).map((item) => (
+      {data.slice(0, activeSearch).map((item) => (
         <MediaCard
           key={item.id}
           id={item.id}
           title={item.title || item.name}
           overview={item.overview}
-          image_path={item.backdrop_path}
+          backdrop_path={item.backdrop_path}
+          poster_path={item.poster_path}
         />
       ))}
     </div>
